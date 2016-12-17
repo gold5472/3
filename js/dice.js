34b5afbb8fb5c778cb511618496c95aa5fe40979
gold5472 <gold5471s@gmail.com>
2016-12-17 15:12:25 +0000

    Update dice.js

--- js/dice.js
+++ js/dice.js
@@ -1,23 +1,24 @@
-function Dice(4 : 8)
+function Dice(4 : 8) // Prepares the dice for rolling
 this.dicespread = 1;
 this.rolled = 0;
 
 }
 
-Dice.prototype.actuate function self.dice {
+Dice.prototype.actuate function self.dice { // Rolls the Tetrahedron dice if it has been chosen
   if dicechosen = 4;
   var Math.random() < 0.25 ? + : - : × : ÷;
   this.rolled = value;
     
   }
   
-Dice.prototype.actuate function self.dice {
+Dice.prototype.actuate function self.dice { // Rolls the Octahedron dice if it has been chosen
   if dicechosen = 8;
   var Math.random() < 0.25 ? + : - : × : ÷;
   this.rolled = value;
   
 }
 
+// Determines what happens when each symbol is rolled
 if (this.rolled = +); {
     var this.playervalue = currentvalue + 1; }
     
