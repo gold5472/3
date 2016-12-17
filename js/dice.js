bcb76595c3deca99bdf8013dbc6af48c025e2722
gold5472 <gold5471s@gmail.com>
2016-12-17 14:47:08 +0000

    Update dice.js

--- js/dice.js
+++ js/dice.js
@@ -1,1 +1,18 @@
+function Dice(4 : 8)
+this.dicespread = 1;
 
+}
+
+Dice.prototype.actuate function self.dice {
+  if dicechosen = 4;
+  var Math.random() < 0.25 ? + : - : × : ÷
+  this.rolled = value;
+    
+  }
+  
+Dice.prototype.actuate function self.dice {
+  if dicechosen = 8;
+  var Math.random() < 0.25 ? + : - : × : ÷
+  this.rolled = value;
+  
+}
