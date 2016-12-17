c50d733488777f7b7c2692d9e90407d752b945eb
gold5472 <gold5471s@gmail.com>
2016-12-17 14:56:33 +0000

    Update dice.js

--- js/dice.js
+++ js/dice.js
@@ -6,17 +6,30 @@ this.rolled = 0;
 
 Dice.prototype.actuate function self.dice {
   if dicechosen = 4;
-  var Math.random() < 0.25 ? + : - : × : ÷
+  var Math.random() < 0.25 ? + : - : × : ÷;
   this.rolled = value;
     
   }
   
 Dice.prototype.actuate function self.dice {
   if dicechosen = 8;
-  var Math.random() < 0.25 ? + : - : × : ÷
+  var Math.random() < 0.25 ? + : - : × : ÷;
   this.rolled = value;
   
 }
 
-if (this.rolled = +);
-    var
+if (this.rolled = +); {
+    var this.playervalue = currentvalue + 1;
+    
+if (this.rolled = -); {
+    var this.playervalue = currentvalue - 1;
+  
+if (this.rolled = ×); {
+    var this.playervalue = currentvalue + 1;
+    var this.otherplayervalue = currentvalue - 1;
+  
+if (this.rolled = ÷); {
+    var this.playervalue = currentvalue - 1;
+    var this.otherplayervalue = currentvalue + 1;
+  
+}
