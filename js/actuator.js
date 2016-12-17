f95a55ffd092be8a5feb2ce0cf48171e4e66be21
gold5472 <gold5471s@gmail.com>
2016-12-17 13:44:06 +0000

    Create actuator.js

---
+++ js/actuator.js
@@ -0,0 +1,16 @@
+function Actuator() {
+this.action     = document.querySelector(".action");
+this.state      = document.querySelector(".state");
+
+}
+
+Actuator.prototype.actuate = function (dice, metadata) {
+var self = this;
+
+}
+});
+
+if (metadata = this.over) {
+self.data = false;
+} else if (metadata = this.won) {
+self.data = true;
